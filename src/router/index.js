5257e0f18e388ea7bfdaf83c48b2f5ad16d0626c
GipsyDanger-fan <2711983537@qq.com>
2020-02-06 16:20:28 +0800

    完成表单校验及绘制

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from '@/views/login'
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes:[]
+  routes:[
+    {path:'/login',component:login}
+  ]
 })
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container-login">
+    <el-card class="my-card">
+      <img src="../../assets/logo_index.png" alt />
+      <el-form status-icon :rules="loginRouter" :model="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "app-login",
+  data() {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        callback(new Error("手机号码格式错误"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        mobile: "",
+        code: ""
+      },
+      loginRouter: {
+        mobile: [
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          { validator: checkMobile, trigger: "blur" }
+        ],
+        code: [
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          { len: "6", message: "请输入验证码", trigger: "blur" }
+        ]
+      }
+    };
+  }
+};
+</script>
+<style scoped lang='less'>
+.container-login {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  left: 0;
+  top: 0;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    transform: translate(-50%, -60%);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto 20px;
+    }
+  }
+}
+</style>
